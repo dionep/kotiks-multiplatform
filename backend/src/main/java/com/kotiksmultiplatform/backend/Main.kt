54e83590d345a46704f86b24ffde705ba41441d8
Rakhimulin Damir <damir@startted.com>
2021-02-04 11:58:23 +0300

    Backend MainKt refactoring

--- backend/src/main/java/com/kotiksmultiplatform/backend/Main.kt
+++ backend/src/main/java/com/kotiksmultiplatform/backend/Main.kt
@@ -4,19 +4,28 @@ import com.google.gson.Gson
 import com.kotiksmultiplatform.backend.entities.Fact
 import io.ktor.server.engine.*
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/facts")  {
-                call.respondText(
-                    Gson().toJson(listOf(Fact("1", "Fact #1"))), ContentType.Application.Json
-                )
-            }
+    embeddedServer(
+        factory = Netty,
+        port = 8080,
+        module = Application::module
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/facts")  {
+            call.respondText(
+                Gson().toJson(listOf(Fact("1", "Fact #1"))), ContentType.Application.Json
+            )
         }
-    }.start(wait = true)
+    }
 }
